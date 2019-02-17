caf1f81e9d1a2a721933a7bf1f06b9dc8afc74aa
Pramod Reddy <38864370+pramodredyy@users.noreply.github.com>
2019-02-17 22:37:37 +0530

    Add files via upload
    It is a python program that implements the SHA algorithm i.e SHA512...etc. The input is encrypted and then a hash is created based on the chosen algorithm

---
+++ SHA.py
@@ -0,0 +1,16 @@
+'''
+import hashlib
+m = hashlib.sha256()
+a=input("enter the word:")
+#b=bytes(a,('utf-8'))
+c=m.update(a.encode('utf-8'))
+print(m.digest())
+'''
+
+import hashlib
+
+a=input("enter the word:")
+c=bytes(a,'utf-8')
+hash_obj=hashlib.sha224(c)    #can use sha1,sha512,sha256,sha512 in place of sha1 we can use md5
+b=hash_obj.hexdigest()
+print(b)
